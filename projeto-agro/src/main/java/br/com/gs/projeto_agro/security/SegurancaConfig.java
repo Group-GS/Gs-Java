d48b8a50fe6d929f519538dfbe408a44d6f4c22d
samyrcouto <costosinho50@gmail.com>
2026-06-07 02:00:05 -0300

    Feat: Adicionando Security ao projeto

---
+++ projeto-agro/src/main/java/br/com/gs/projeto_agro/security/SegurancaConfig.java
@@ -0,0 +1,36 @@
+package br.com.gs.projeto_agro.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SegurancaConfig {
+
+    @Autowired
+    private JWTAuthFilter authFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/autenticacao/**", "/swagger-ui/**", "/v3/**")
+                .permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
